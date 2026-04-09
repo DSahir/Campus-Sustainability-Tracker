06e50db9ab5ee3f5cb3c6ac51c884f1826cc4fa8
Srivarsh Cirigiri <scirigiri@umass.edu>
2026-04-09 17:17:07 -0400

    Resolved merge conflict

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI
 from auth import router as auth_router
+from models import Base
+from sqlalchemy import create_engine
+import time
 
 app = FastAPI()
 
+# Include auth routes
 app.include_router(auth_router)
 
+DATABASE_URL = "postgresql://user:password@db:5432/sustainability"
+
+# Retry DB connection
+for i in range(10):
+    try:
+        engine = create_engine(DATABASE_URL)
+        Base.metadata.create_all(bind=engine)
+        print("Connected to DB")
+        break
+    except Exception as e:
+        print("Waiting for DB...")
+        time.sleep(3)
+
 @app.get("/")
-def root():
-    return {"message": "Backend running"}
+def read_root():
+    return {"message": "Backend is running!"}
